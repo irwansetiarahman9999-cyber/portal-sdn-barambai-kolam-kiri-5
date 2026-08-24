a22851add1b3da46db69f6faff712bbd92a46180
irwansetiarahman9999-cyber <irwansetiarahman9999@gmail.com>
2026-08-24 10:45:21 +0800

    Delete seed.sql

--- seed.sql
+++
@@ -1,2 +0,0 @@
-INSERT IGNORE INTO roles (id, role_name) VALUES (1, 'SUPER_ADMIN'), (2, 'ADMIN'), (3, 'OPERATOR'), (4, 'EDITOR'), (5, 'GURU');
-INSERT IGNORE INTO users (id, username, email, password_hash, role_id, status) VALUES ('user-uuid-superadmin', 'superadmin', 'admin@sdnbarambai.sch.id', '\\\.n8D807csWj.962ZO13S7d4PjGZ1K7D7/96Rpoz.7PZ5S3kq', 1, 'ACTIVE'); -- password is 'admin123'
